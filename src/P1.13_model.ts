11cd3b7aaf4491f531db545bfb4f08a9312d5b99
Guilherme <guilhermecardosot3@gmail.com>
2024-03-18 02:23:07 -0300

    Prototipo

---
+++ src/P1.13_model.ts
@@ -0,0 +1,20 @@
+/* P1.13 Modele, utilizando orientação a objetos, um usuário de um sistema que tenha
+preenchido as seguintes informações em uma interface de cadastro: nome, ano de nascimento,
+cpf e gênero. Esse usuário deve possuir um método chamado equals, que compara uma instância
+da classe com outra passada como argumento para o método equals e outro método chamado
+speak_name que retorna a string representada no nome do usuário. Demonstre a execução de
+um programa que faça uso dessa classe, exibindo resultados no console.*/
+
+import { Usuario } from "./P1.13_view";
+
+// Demonstrar a execução do programa
+const usuario1 = new Usuario("Test1", 9999, "000.000.000-00", "Masculino");
+const usuario2 = new Usuario("Maria", 1985, "987.654.321-00", "Feminino");
+
+console.log(usuario1.speak_name()); // Saída: Meu nome é João
+console.log(usuario2.equals(usuario1)); // Saída: false, pois são usuários diferentes
+
+// Criando um usuário idêntico ao usuário1 para demonstrar o método equals
+const usuario3 = new Usuario("João", 1990, "123.456.789-00", "Masculino");
+console.log(usuario3.equals(usuario1)); // Saída: true, pois são usuários iguais
+
---
+++ src/P1.13_view.ts
@@ -0,0 +1,24 @@
+export class Usuario {
+    nome: string;
+    ano_nascimento: number;
+    cpf: string;
+    genero: string;
+
+    constructor(nome: string, ano_nascimento: number, cpf: string, genero: string) {
+        this.nome = nome;
+        this.ano_nascimento = ano_nascimento;
+        this.cpf = cpf;
+        this.genero = genero;
+    }
+
+    equals(outroUsuario: Usuario): boolean {
+        return this.nome === outroUsuario.nome &&
+               this.ano_nascimento === outroUsuario.ano_nascimento &&
+               this.cpf === outroUsuario.cpf &&
+               this.genero === outroUsuario.genero;
+    }
+
+    speak_name(): string {
+        return `Meu nome é ${this.nome}`;
+    }
+}
